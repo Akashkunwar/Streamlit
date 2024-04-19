0d8e548fb0dc15373b488738170488500447f018
Akash Kumar <gnspda@gmail.com>
2024-04-19 23:26:22 +0530

    Basics

---
+++ Basics/app.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import time
+st.title ("this is the app title")
+st.header("this is the markdown")
+st.markdown("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r''' a+a r^1+a r^2+a r^3 ''')
+st.write("Hello ,let's learn how to build a streamlit app together")
+st.image("kid.jpg")
+st.audio("audio.mp3")
+st.video("video.mp4")
+st.checkbox('yes')
+st.button('Click')
+st.radio('Pick your gender',['Male','Female'])
+st.selectbox('Pick your gender',['Male','Female'])
+st.multiselect('choose a planet',['Jupiter', 'Mars', 'neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.slider('Pick a number', 0,50)
+st.number_input('Pick a number', 0,10)
+st.text_input('Email address')
+st.date_input('Travelling date')
+st.time_input('School time')
+st.text_area('Description')
+uploaded_file = st.file_uploader('Upload a photo')
+if uploaded_file is not None:
+    print("Uploaded file:", uploaded_file.name)
+    # Do something with the file, like saving it
+    with open(uploaded_file.name, 'wb') as f:
+        f.write(uploaded_file.getvalue())
+st.color_picker('Choose your favorite color')
+# st.balloons()
+st.progress(10)
+with st.spinner('Wait for it...'):    
+    time.sleep(1)
+st.success("You did it !")
+st.error("Error")
+st.warning("Warning")
+st.info("It's easy to build a streamlit app")
+st.exception(RuntimeError("RuntimeError exception"))
+
+
+
+import streamlit as st
+import pandas as pd
+from io import StringIO
+
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    # To read file as bytes:
+    bytes_data = uploaded_file.getvalue()
+    st.write(bytes_data)
+
+    # To convert to a string based IO:
+    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+    st.write(stringio)
+
+    # To read file as string:
+    string_data = stringio.read()
+    st.write(string_data)
+
+    # Can be used wherever a "file-like" object is accepted:
+    dataframe = pd.read_csv(uploaded_file)
+    print(dataframe)
+    st.write(dataframe)
